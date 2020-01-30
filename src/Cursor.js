527215f977f7b790d13df1a66ac25db364db4116
bselpin <bselpin@naver.com>
2020-01-30 11:38:17 +0900

    no message

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,61 +1,49 @@
 import React from 'react';
 import './css/Cursor.scss';
 
-class Cursor extends React.Component {
-    componentDidMount() {
-        const cursor = document.querySelector('.cursor')
-        const aTag = document.getElementsByClassName('tag')
-        const darkTag = document.getElementsByClassName('dark')
-        const map = document.getElementById('map')
+setTimeout(() => {
+    const cursor = document.querySelector('.cursor')
+    const aTag = document.getElementsByClassName('tag')
+    const darkTag = document.getElementsByClassName('dark')
 
-        for (let i = 0; i < aTag.length; i++) {
-            aTag[i].addEventListener('mouseover', () => {
-                cursor.classList.add('hover')
-            })
-            aTag[i].addEventListener('mouseleave', () => {
-                cursor.classList.remove('hover')
-            })
-        }
-
-        for (let i = 0; i < darkTag.length; i++) {
-            darkTag[i].addEventListener('mouseover', () => {
-                cursor.classList.add('dark')
-            })
-            darkTag[i].addEventListener('mouseleave', () => {
-                cursor.classList.remove('dark')
-            })
-        }
-
-        map.addEventListener('mouseover', () => {
-            cursor.classList.add('map')
+    for (let i = 0; i < aTag.length; i++) {
+        aTag[i].addEventListener('mouseover', () => {
+            cursor.classList.add('hover')
         })
-
-        map.addEventListener('mouseleave', () => {
-            cursor.classList.remove('map')
-        })
-
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute(
-                "style", `transform: translate3d(${e.clientX - 20}px, ${e.clientY - 20}px, -10px);`)
-        })
-
-        document.addEventListener('mousedown', () => {
-            cursor.classList.add('expand')
-        })
-
-        document.addEventListener('mouseup', () => {
-            cursor.classList.remove('expand')
-        })
+        aTag[i].addEventListener('mouseleave', () => {
+            cursor.classList.remove('hover')
+        })        
     }
 
-    render() {
-        return (
-            <>
-                <div className="cursor"></div>
-            </>
-        )
+    for (let i = 0; i < darkTag.length; i++) {
+        darkTag[i].addEventListener('mouseover', () => {
+            cursor.classList.add('dark')
+        })
+        darkTag[i].addEventListener('mouseleave', () => {
+            cursor.classList.remove('dark')
+        })        
     }
-    
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute(
+            "style", `transform: translate3d(${e.clientX - 20}px, ${e.clientY - 20}px, -10px);`)
+    })
+
+    document.addEventListener('mousedown', () => {
+        cursor.classList.add('expand')
+    })
+
+    document.addEventListener('mouseup', () => {
+        cursor.classList.remove('expand')
+    })
+}, 10);
+
+function Cursor() {
+    return (
+        <>  
+            <div className="cursor"></div>          
+        </>
+    )
 }
 
 export default Cursor
